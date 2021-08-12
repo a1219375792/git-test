e975e49c4b35e39ffded64191f7db780bc244810
LBF <LBF@qq.com>
2021-08-12 21:38:14 +0800

    Master Commit3

--- src/main/java/com/bjpowernode/git/GitTest.java
+++ src/main/java/com/bjpowernode/git/GitTest.java
@@ -2,6 +2,6 @@ package com.bjpowernode.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Hot-Fix分支");
+        System.out.println("Master分支");
     }
 }
